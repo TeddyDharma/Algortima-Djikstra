4bac89ab5f32b0353173a847fb0b57b0cc7f1a6a
TeddyDharma <107167531+TeddyDharma@users.noreply.github.com>
2022-06-17 09:50:24 +0800

    Update Dijkstra Algo final.cpp

--- Dijkstra Algo final.cpp
+++ Dijkstra Algo final.cpp
@@ -1,68 +1,80 @@
-#include <bits/stdc++.h>
-using namespace std;
-int jarak_vertex[20][20]; //mendeklarasikan jarak antara satu vertex dengan vertex yang lain
-int vertex;
-int minimum_vertex(int jarak[], bool dikunjungi[]){
-	int node; 
-	int min = INT_MAX; //mengisi variabel min dengan nilai maksimal dari integer yaitu sekitar 2 milyar
-	for(int i = 0; i < vertex; i++){
-		if(dikunjungi[i] == false and jarak[i] <= min){ //jika node nya belum dikunjungi dan jarak dari satu node ke node lain kurang atau sama dengan nilai min maka masuk ke dalam
-			//pengkonisian ini
-			min = jarak[i]; //variabel min akan menampung jarak dari node satu dengan node yang lain 
-			node = i; // varibel node akan menyimpan nilai dari i, dimana i merupakan node yang tersambung dengan node yang terpilih namun dengan bobot terkecil
+#include <stdio.h>
+#include <conio.h>
+#define N 5 //asumsikan dulu ada 5 buah titik yang ada di dalam kota
+#define LIM 999
+int main(){
+	int matriks[N][N]; //menampung matriks tetangga
+	int asal; //menentukan titik awal untuk menentukan jarak
+	int tempMatriks[N][N]; //variabel yang akan menyimpan nilai matrik yang sudah dilakuan modifikasi dengan nilai LIM
+	int jarak[N]; // menampung jarak dari node asal ke node yang lain
+	int temp[N]; 
+	int visit[N];//menentukan apakah node sudah dikunjugi atau blm    
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			printf("ketik jarak titik %d dengan titik %d (dalam km) : ", i + 1, j + 1);
+			scanf("%d", &matriks[i][j]); //memasukan bobot antar vertex
 		}
 	}
-	return node; //setelah dilakukan perulangan maka program akan mengembalikan nilai dari variabel node yang dimana variabel node akan berisi vertex yang berhubungan 
-	//namun dengan bobot yang terkecil
-}
-void tampilkan_solusi(int jarak[], int par[]){
-	for(int i = 0; i < vertex; i++){
-		int simpan; 
-		simpan = par[i];
-		while(simpan != -1){
-			cout << simpan << " <- "; 
-			simpan = par[simpan];
-		}
-		cout << endl;
-		cout <<"jarak minumum : " << jarak[i];
-		cout << endl;
-	}
-
-}
-void dijkstra(int node_pilihan){
-	int par[10]; //array par akan menampung vertex mana saja yang sudah dilakukan pengunjungan atau dengan kata lain menyimpan vertex mana saja yang sudah dihubungi
-	int jarak[10]; // array jarak akan menampung jarak dari setiap vertex
-	bool dikunjungi[10] = {0}; // arrray dikunjungi diisi dengan nilai 0 yang  menandakan bahwa semua vertex belum pernah dikunjungi sebelumnya
-	fill(jarak, jarak + vertex, INT_MAX); //menggunaakan fungsi file yang merupakan bagian dari file header bits/c++.h 
-	jarak[node_pilihan] = 0; //menginisiasi jarak vertex yang dipilih terhadap dirinya sendiri adalah 0
-	par[node_pilihan] = -1; // array par akan bernilai - 1 yang menandakan bahwa node pilihan sudah pernah dikunjugi
-	for(int i = 1; i <= vertex; i++){
-		int get_min = minimum_vertex(jarak, dikunjungi); // mencari vertex lain yang terhubung dengan vertex pilihan kemudian mencari bobot minimumnya dan kemudian hasilnya disimpan di dalam variael get_min
-		dikunjungi[get_min] = true; //vertex yang telah dikunjungi akan diisi dengan nilai true
-		cout <<"minimum : " << get_min << endl;  
-		for(int j = 0; j < vertex; j++){
-			if(!dikunjungi[j] and (jarak[get_min] + jarak_vertex[get_min][j]) < jarak[j] and jarak_vertex[get_min][j] != 9999){ //jika hasil penjumlahan jarak node sebelumya dengan jarak_vertex[get_min][j] terhadap jarak[j] 
-				//jarak_vertex[get_min][j] nilai nya tidak sama dengan 9999
-				par[j] = get_min; //maka node yang ditampung di dalam variabel get_min akan diatampung di dalam arrray par
-				jarak[j] = jarak[get_min] + jarak_vertex[get_min][j]; // kemudian jarak menuju j akan dilakukan operasi penambahan
+	printf("ketik titik asal anda mengirim : "); //masukan node awal / posisi awal
+	scanf("%d",&asal);
+	printf("==========================================\n");  
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if(matriks[i][j] == 0){
+				tempMatriks[i][j] = LIM; //inisiasi dengan nilai LIM jika bobot matriks[i][j] bernilai 0
+			}else{
+				tempMatriks[i][j] = matriks[i][j]; // jika tidak bernilai 0 maka simpan di dalam tempMatriks
 			}
 		}
 	}
-	tampilkan_solusi(jarak, par); //menampilkan semua soluisi yang ada
-}
-int main(){
-	int simpul; 
-	int node_awal;
-	cout <<"ketik banyak simpul yang anda inginkan : "; 
-	cin >> simpul; 
-	for(int i = 0; i < simpul; i++){
-		for(int j = 0; j < simpul; j++){
-			cout <<"ketik bobot vertex " << i << " dengan " << j << " : "; 
-			cin >> jarak_vertex[i][j]; 
+	for(int i = 0; i < N; i++){
+		jarak[i] = tempMatriks[asal][i]; // berisikan jarak node asal dengan node ke i 
+		temp[i] = asal; // tampung nilai node asal ke dalam variabel temp
+		visit[i] = 0; //menandakan bahwa belum sempat dikunjungi; 
+
+	}
+	jarak[asal] = 0;  // inisialisasi jarak[awal] dengan nilai 0 sebagai indikasi tidak adanya gelang
+	visit[asal] = 1;  // inisialisasi visit[awal] dengan nilai 1 sebagai indikasi node sudah pernah dikunjungi
+	int hitung_node = 1; // inisialisasi variabel hitung node karene node asal sudah dikunjungi sehingga hitung node bernilai 1
+	int jarak_min , next_node;
+	int nextvertex;
+	while(hitung_node < N - 1){
+		jarak_min = LIM; 
+		for(int i = 0; i < N; i++){
+			if(jarak[i] < jarak_min && visit[i] != 1){ // jika memnuhi kondisi ini maka lakukan perintah yang ada di dalamnya 
+				jarak_min = jarak[i]; // tampung jarak[i] ke dalam jarak_min
+				nextvertex = i; //kemudian program akan memilih vertex ke yang menjadi pilihan dalam jarak_min untuk menjadi acuan yang baru
+			}
+		}
+		visit[nextvertex]  = 1; // vertex yang menjadi acuan yang baru diisi nilai nya dengan 1 yang menandakan sudah dikunjungi 
+		for(int i = 0; i < N; i++){
+			if(visit[i] != 1){
+				if(jarak_min + tempMatriks[nextvertex][i] < jarak[i]){ //bandingkan hasil penjumalahan jarak antar node dengan nilai yang ada di dalam array jarak
+					jarak[i] = jarak_min + tempMatriks[nextvertex][i];
+					temp[i] = nextvertex; // menyimpan semua node yang jika terhubung akan menghasilkan jarak minimum dari lintasan awal ke lintasan yang dituju 
+				}
+			}
+		}
+		hitung_node++; 
+	}
+	int a[N+1], k, j; 
+	for(int i = 0; i < N; i++){
+		if(i != asal){ //menampilkan jarak sebuah node dengan node yang lain disertai dengan jalur yang harus dilalui 
+			printf("jarak titik asal %d ke titik %d adalah : %d\n", asal, i, temp[i]);
+			printf("jalur yang harus ditempuh : "); 
+			j = i;
+			printf("%d <- ", i);
+			while(j != asal){
+				j = temp[j]; // variabel temp sebagai variabel yang menyimpan vertex yang terpilih menjadi nextvertex
+				printf("%d", j); 
+				if(j != asal){
+					printf("<-"); 
+				}
+			}
+			printf("\n");
 		}
 	}
-	cout <<"ketik node awal : "; 
-	cin >> node_awal; 
-	dijkstra(node_awal); 
-	return 0;
+	printf("jarak titik asal %d ke titik %d adalah : %d\n", asal, N - 1, jarak[N-1]); 
+	getch(); 
+	return 0; 
 }
